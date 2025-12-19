eb20bd2fdf90905958bcbc92faddca9f357b98c4
aibocah <aibocah86@gmail.com>
2025-12-19 19:27:49 +0700

    Add files via upload

---
+++ lib/theme/dark_theme.dart
@@ -0,0 +1,21 @@
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static ThemeData dark = ThemeData(
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: const Color(0xFF121212),
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color(0xFF121212),
+      elevation: 0,
+    ),
+    inputDecorationTheme: InputDecorationTheme(
+      filled: true,
+      fillColor: const Color(0xFF1E1E1E),
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(12),
+        borderSide: const BorderSide(color: Color(0xFF3A3A3A)),
+      ),
+    ),
+  );
+}
