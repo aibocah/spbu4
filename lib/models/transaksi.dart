5c66410a2899cc2d7b3dffa9f74c27638e85c3c7
aibocah <aibocah86@gmail.com>
2025-12-19 19:21:31 +0700

    Add files via upload

---
+++ lib/models/transaksi.dart
@@ -0,0 +1,22 @@
+
+class Transaksi {
+  int noTrans;
+  String operator;
+  String bbm;
+  double harga;
+  double liter;
+  double total;
+  String plat;
+  String waktu;
+
+  Transaksi({
+    required this.noTrans,
+    required this.operator,
+    required this.bbm,
+    required this.harga,
+    required this.liter,
+    required this.total,
+    required this.plat,
+    required this.waktu,
+  });
+}
