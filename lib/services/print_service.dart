aeb5c2889fc8efa1fdc7b2048ab440468d20dc88
aibocah <aibocah86@gmail.com>
2025-12-20 17:32:50 +0700

    Refactor print service to use BlueThermalPrinterPlus
    Updated printer implementation to use BlueThermalPrinterPlus and fixed text styling.

--- lib/services/print_service.dart
+++ lib/services/print_service.dart
@@ -4,15 +4,17 @@ import 'package:esc_pos_utils/esc_pos_utils.dart';
 import '../models/transaksi.dart';
 
 class PrintService {
-  static final printer = BlueThermalPrinter.instance;
+  import 'package:blue_thermal_printer_plus/blue_thermal_printer_plus.dart';
+final BlueThermalPrinterPlus printer = BlueThermalPrinterPlus.instance;
+
 
   static Future<void> printStruk(Transaksi t) async {
     final profile = await CapabilityProfile.load();
     final gen = Generator(PaperSize.mm58, profile);
 
     List<int> bytes = [];
-    bytes += gen.text('PERTAMINA', styles: PosStyles(bold: true, align: PosAlign.center));
-    bytes += gen.text('SPBU HM. SULCHAN', align: PosAlign.center);
+    bytes += gen.text('PERTAMINA', styles: PosStyles(bold: true, styles: const PosStyles(align: PosAlign.center),);
+    bytes += gen.text('SPBU HM. SULCHAN', styles: const PosStyles(align: PosAlign.center),);
     bytes += gen.hr();
     bytes += gen.text('BBM     : ${t.bbm}');
     bytes += gen.text('Volume  : ${t.liter.toStringAsFixed(2)} L');
@@ -21,7 +23,8 @@ class PrintService {
     bytes += gen.text('CASH');
     bytes += gen.text('Plat: ${t.plat}');
     bytes += gen.hr();
-    bytes += gen.text('Subsidi BBM dari Pemerintah', align: PosAlign.center);
+    bytes += gen.text('Subsidi BBM dari Pemerintah', styles: const PosStyles(align: PosAlign.center),
+);
     bytes += gen.cut();
 
     if (!(await printer.isConnected ?? false)) {
